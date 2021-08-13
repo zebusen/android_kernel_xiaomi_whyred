bb624789aaaa291dbf282f399bd7d19dc187520c
zebusen <zebusensei@gmail.com>
2021-08-13 12:40:42 +0800

    Revert zram dedup 57242b538c72d553efe76d3d65d2c3dbe0bfbfdc to 22fdd03ff87c33b270edabe597793157b8ca2c55.

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -18,10 +18,8 @@
 #include <linux/rwsem.h>
 #include <linux/zsmalloc.h>
 #include <linux/crypto.h>
-#include <linux/spinlock.h>
 
 #include "zcomp.h"
-#include "zram_dedup.h"
 
 #define SECTORS_PER_PAGE_SHIFT	(PAGE_SHIFT - SECTOR_SHIFT)
 #define SECTORS_PER_PAGE	(1 << SECTORS_PER_PAGE_SHIFT)
@@ -56,18 +54,10 @@ enum zram_pageflags {
 
 /*-- Data structures */
 
-struct zram_entry {
-	struct rb_node rb_node;
-	u32 len;
-	u32 checksum;
-	unsigned long refcount;
-	unsigned long handle;
-};
-
 /* Allocated for each disk page */
 struct zram_table_entry {
 	union {
-		struct zram_entry *entry;
+		unsigned long handle;
 		unsigned long element;
 	};
 	unsigned long value;
@@ -89,16 +79,6 @@ struct zram_stats {
 	atomic64_t pages_stored;	/* no. of pages currently stored */
 	atomic_long_t max_used_pages;	/* no. of maximum pages stored */
 	atomic64_t writestall;		/* no. of write slow paths */
-	atomic64_t dup_data_size;	/*
-					 * compressed size of pages
-					 * duplicated
-					 */
-	atomic64_t meta_data_size;	/* size of zram_entries */
-};
-
-struct zram_hash {
-	spinlock_t lock;
-	struct rb_root rb_root;
 };
 
 struct zram {
@@ -106,8 +86,6 @@ struct zram {
 	struct zs_pool *mem_pool;
 	struct zcomp *comp;
 	struct gendisk *disk;
-	struct zram_hash *hash;
-	size_t hash_size;
 	/* Prevent concurrent execution of device init */
 	struct rw_semaphore init_lock;
 	/*
@@ -126,7 +104,6 @@ struct zram {
 	 * zram is claimed so open request will be failed
 	 */
 	bool claim; /* Protected by bdev->bd_mutex */
-	bool use_dedup;
 #ifdef CONFIG_ZRAM_WRITEBACK
 	struct file *backing_dev;
 	struct block_device *bdev;
@@ -139,15 +116,4 @@ struct zram {
 	struct dentry *debugfs_dir;
 #endif
 };
-
-static inline bool zram_dedup_enabled(struct zram *zram)
-{
-#ifdef CONFIG_ZRAM_DEDUP
-	return zram->use_dedup;
-#else
-	return false;
-#endif
-}
-
-void zram_entry_free(struct zram *zram, struct zram_entry *entry);
 #endif
